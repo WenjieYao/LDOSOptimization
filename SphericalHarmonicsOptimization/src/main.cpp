763920c56c56d5819a0c27b3736867654043b2fa
Wenjie Yao <jayyao@mit.edu>
2019-01-14 04:22:42 -0500

    Convert Spheroid to Sperical Harmonics

--- SphericalHarmonicsOptimization/src/main.cpp
+++ SphericalHarmonicsOptimization/src/main.cpp
@@ -23,7 +23,7 @@ int fcount = 0;
 /***************************************************************/
 /***************************************************************/
 double lambda_0 = 550;                          // wavelength in unit of nm
-const unsigned int num_coeffs = 16+1;           // number of coefficients
+const unsigned int num_coeffs = 5*5+1;           // number of coefficients
 double k_0 = 2*pi/lambda_0;                     // wavenumber in unit of 1/nm
 cdouble Chi = 0;                                // chi = epsilon-1
 double d_min = 100;                             // minimum distance in unit of nm
@@ -74,6 +74,51 @@ double myfunc(const std::vector<double> &x, std::vector<double> &grad, void *my_
   results_file.close();
 }
 
+double SpheroidRadius(double theta, double r1, double r2){
+  return r1*r2/sqrt(r1*r1*cos(theta)*cos(theta)+r2*r2*sin(theta)*sin(theta));
+}
+
+void SpheroidToHarmonics(double *coeff, unsigned int num_coeff, double r1, double r2){
+  geometry::SphericalHarmonics *sh;  //create spherical harmonic class
+  double *temp_coeffs = new double[num_coeff];
+  for(int i=0;i<num_coeff;++i)
+    temp_coeffs[i] = 1;
+  sh=new geometry::SphericalHarmonics(num_coeff, temp_coeffs, -1e6, 1e6);
+  double *l=new double[num_coeff];
+  double *m=new double[num_coeff];
+  sh->get_lm(l,m);
+  //create integral theta and phi
+  unsigned int Nt = 500;
+  unsigned int Np = 2*Nt;
+  double *Theta = new double[Nt];
+  double *Phi = new double[Np];
+  double dt = 1.0*pi/(Nt-1);
+  double dp = 2.0*pi/(Np-1);
+  for(int i=0;i<Nt;++i)
+    Theta[i] = dt*i;
+  for(int i=0;i<Np;++i)
+    Phi[i] = dp*i;
+  //loop for integral
+  for(int n=0;n<num_coeff;++n){
+    //std::cout << n << " ";
+    double cn = 0;
+    for(int p=0;p<Np;++p){
+      for(int t=0;t<Nt;++t){
+        double theta = Theta[t];
+        double phi  = Phi[p];
+        cn += (1+(m[n]==0)*1)*0.5*sqrt(SpheroidRadius(theta,r1,r2)-d_min/1e3)*sh->Ylm(l[n],m[n],theta,phi)*sin(theta)*dt*dp;
+        //cn += sh->Ylm(l[n+1],m[n+1],theta,phi)*sh->Ylm(l[n],m[n],theta,phi)*sin(theta)*dt*dp;
+      }
+    }
+    //std::cout << cn << std::endl;
+    coeff[n] = cn;
+  }
+  delete[] Theta;
+  delete[] Phi;
+  delete[] l;
+  delete[] m;
+  delete sh;
+}
 
 int main(){
   ofstream results_file;                          //write to file
@@ -86,8 +131,8 @@ int main(){
   std::vector<double> lb(Nc-1);
    std::vector<double> ub(Nc-1);
   for (int i=0;i<Nc-1;++i){
-    lb[i] = 0;
-    ub[i] = (i<=4)*4+2;
+    lb[i] = -10;
+    ub[i] = 10;
   }
   opt.set_lower_bounds(lb);
   opt.set_upper_bounds(ub);
@@ -98,12 +143,18 @@ int main(){
   opt.set_xtol_abs(1e-5);
   //opt.set_xtol_rel(0);
   //opt.get_maxeval();
-
+ std::cout << "Start converting spheroid to spherical harmonics" << std::endl;
   std::vector<double> x(Nc-1);
-  //x[0]=d_min/1e3*4*pi;                          // initial value Ylm(0,0,0,0)^2 = 4pi
-  for(int i=1;i<Nc;++i)
-    x[i-1] = 0.1/i;
-  x[0] = 0.1;
+  double *coeff = new double[Nc-1];
+  double r1 = 190/1e3;
+  double r2 = d_min/1e3;
+  for(int i=0;i<Nc-1;++i)
+    coeff[i] = 0;
+  SpheroidToHarmonics(coeff,Nc-1,r1,r2);
+  //x[0]=d_min/1e3*4*pi;                          // initial value 
+  for(int i=0;i<Nc-1;++i){
+    x[i] = coeff[i];
+  }
   double maxf;
   std::cout << "Starting optimization... " << std::endl;
   nlopt::result result = opt.optimize(x, maxf);
@@ -118,4 +169,5 @@ int main(){
   double rho_limit = pow(k_0*d_min,-3)*abs(Chi*Chi)/imag(Chi)*(1+pow(k_0*d_min,2));
   std::cout << "d_min: " << d_min << std::endl;
   std::cout << "rho_limit: " << rho_limit << std::endl;
+  delete coeff;
 }
