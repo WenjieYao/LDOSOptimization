f187dbba4666119e13105e5f2a382548ca7cc8fb
Wenjie Yao <jayyao@mit.edu>
2019-01-14 02:01:53 -0500

    some results

--- SphericalHarmonicsOptimization/src/others/DerValid.cpp
+++ SphericalHarmonicsOptimization/src/others/DerValid.cpp
@@ -27,29 +27,30 @@ int main()
   double k_0 = 2*pi/lambda_0;                   // wavenumber in unit of 1/nm
   cdouble Chi = 0;                              // chi = epsilon-1
   double d_min = 200;                           // minimum distance in unit of nm
-  const unsigned int num_coeffs = 9;            // number of coefficients
+  const unsigned int num_coeffs = 9+1;            // number of coefficients
   
-  int min_mesh = 1000;
-  int max_mesh = 3500;
+  int min_mesh = 2000;
+  int max_mesh = 5000;
   double *coeffs = new double[Nc];              // array to store coeffs
-  coeffs[0]=d_min/1e3*4*pi;                     // initial value Ylm(0,0,0,0)^2 = 4pi
-  double delta_c = coeffs[0]/500;               // increasement of coefficients used
+  coeffs[0]=d_min/1e3;                     // initial value Ylm(0,0,0,0)^2 = 4pi
+  double delta_c = 1e-2;               // increasement of coefficients used
   for(int i=1;i<Nc;++i)
-    coeffs[i] = 0.00;
-  //coeffs[1] = 1;
+    coeffs[i] = 0.5/i;
+  //coeffs[1] = 0.1;
+  //coeffs[2] = 0.1;
   WriteCoeff(coeffs,num_coeffs);                // write coefficients to file
 
   double resolution = 1.5;                      // resolution, larger the finer, default 1
   
   /****************Loop for different coeff LDOS ***************************/
   VectorXd Rho_s(MAX_ITER);                     // array to store all elecric LDOS
-  MatrixXd Dfdx(Nc,MAX_ITER);                   // matrix to store all derivatives
+  MatrixXd Dfdx(Nc-1,MAX_ITER);                   // matrix to store all derivatives
   VectorXd Dfdx_t(MAX_ITER-1);
   VectorXd Dfdx_s(MAX_ITER-1);
-  MatrixXd Coeffs(Nc,MAX_ITER);                 // matrix to store all coefficients
+  MatrixXd Coeffs(Nc-1,MAX_ITER);                 // matrix to store all coefficients
   double rho_s;                                 // electric LDOS at center computed with 3 scatter simulation
-  double *dfdx = new double[Nc];                // gradient of LDOS vs coeffs
-  for (int i=0;i<Nc;++i)                        // gradient initialzed to 0
+  double *dfdx = new double[Nc-1];                // gradient of LDOS vs coeffs
+  for (int i=0;i<Nc-1;++i)                        // gradient initialzed to 0
     dfdx[i] = 0.0;
   unsigned int pc = 0;
   /*********** k = 0 *******************************************************/
@@ -58,8 +59,8 @@ int main()
   //Dfdx(0) = dfdx[pc];
   std::cout << "Rho_s: " << Rho_s(0) << std::endl;
   std::cout << "dfdx: " << dfdx[pc] << std::endl;
-  for (int i=0;i<Nc;++i){
-    Coeffs(i,0) = coeffs[i];
+  for (int i=0;i<Nc-1;++i){
+    Coeffs(i,0) = coeffs[i+1];
     Dfdx(i,0) = dfdx[i];
   }
   /**************************************************************************/
@@ -68,15 +69,15 @@ int main()
   for (int k=1;k<MAX_ITER;++k){
     std::cout << std::endl << "iteratoin k = " << k << " out of MAX_ITER: " << MAX_ITER << std::endl;
     rho_s = 0;
-    for (int i=0;i<Nc;++i)                      // gradient initialzed to 0
+    for (int i=0;i<Nc-1;++i)                      // gradient initialzed to 0
       dfdx[i] = 0.0;
     /* intialize coefficients                  *****************************/
-    pc = k%Nc;
-    coeffs[pc] += delta_c;
+    pc = k%(Nc-1);
+    coeffs[pc+1] += delta_c;
     /**************compute the gradient of LDOS at current coeffs **********/
     LDOS_gradient(lambda_0, coeffs, num_coeffs, rho_s, dfdx, Chi,d_min ,min_mesh,max_mesh,resolution);
-    for (int i=0;i<Nc;++i){
-      Coeffs(i,k) = coeffs[i];
+    for (int i=0;i<Nc-1;++i){
+      Coeffs(i,k) = coeffs[i+1];
       Dfdx(i,k) = dfdx[i];
     }
 
