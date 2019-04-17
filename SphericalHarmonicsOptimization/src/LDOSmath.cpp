5db65c0d8ec53041c0e9579c58731b7d8d920f4e
Wenjie Yao <jayyao@mit.edu>
2019-04-17 05:25:53 -0400

    update mesh

--- SphericalHarmonicsOptimization/src/LDOSmath.cpp
+++ SphericalHarmonicsOptimization/src/LDOSmath.cpp
@@ -5,6 +5,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+#include <string.h>
+#include <iomanip>
 #include "scuff-scatter.h"
 #include "basisfunctions.h"
 #include "LDOSmath.h"
@@ -16,7 +19,7 @@
 /***************************************************************/
 
 void WriteCoeff(double *coeffs, const unsigned int num_coeffs){
-  ofstream coeff_file;                          //write to file
+  std::ofstream coeff_file;                          //write to file
   coeff_file.open ("coeff.txt");
   for(int i=0;i<num_coeffs;++i){
     //coeffs[i] *= 2*sqrt(pi);
@@ -36,12 +39,75 @@ bool CompareCoeff(double *coeffs, const unsigned int num_coeffs){
   }
   double diff = 0;
   for(int i=0;i<num_coeffs;++i)
-    diff += pow(Excoeffs[i]-coeffs[i],2);
+    diff += abs(Excoeffs[i]-coeffs[i]);
   bool rv = false;
-  if(diff<1)
+  if(diff>1)
     rv = true;
-  delete Excoeffs;
+  delete[] Excoeffs;
   return rv;
+}
+
+void ReadMSH(std::vector<double> &x, std::vector<double> &y, std::vector<double> &z, std::vector<int> &v1, std::vector<int> &v2, std::vector<int> &v3){
+    std::ifstream file("Ellipsoid.msh");
+    int ni=0;
+    int nNodes=0;
+    int nElements=0;
+    if (file.is_open()) {
+        std::string line;
+        while(std::getline(file,line)){
+            ni++;
+            std::stringstream ss(line);
+            if(ni<5)
+                continue;
+            else if(ni==5)
+                ss >> nNodes;
+            else if((ni<6+nNodes)&&(nNodes!=0)){
+                int n_temp;
+                double x_temp,y_temp,z_temp;
+                ss >> n_temp >> x_temp >> y_temp >> z_temp;
+                //std::cout << x_temp << " " << y_temp << " " << z_temp << std::endl;
+                x.push_back(x_temp);
+                y.push_back(y_temp);
+                z.push_back(z_temp);
+            }
+            else if((ni==(8+nNodes))&&(nNodes!=0))
+                ss >> nElements;
+            else if((ni>(8+nNodes))&&(ni<=(8+nNodes+nElements))){
+                int n_temp;
+                int vt1,vt2,vt3;
+                ss >> n_temp >> n_temp >> n_temp >> n_temp >> n_temp >> vt1 >> vt2 >> vt3;
+                //std::cout << vt1 << " " << vt2 << " " << vt3 << std::endl;
+                v1.push_back(vt1);
+                v2.push_back(vt2);
+                v3.push_back(vt3);
+            }    
+        }
+    file.close();
+  }
+}
+
+void WriteMSH(std::vector<double> &x, std::vector<double> &y, std::vector<double> &z, std::vector<int> &v1, std::vector<int> &v2, std::vector<int> &v3){
+    std::ofstream file("temp.msh");
+    int nNodes=x.size();
+    int nElements=v1.size();
+    if (file.is_open()) {
+        file << std::setprecision(16) << "$MeshFormat\n"; 
+        file << "2.2 0 8\n";
+        file << "$EndMeshFormat\n"; 
+        file << "$Nodes\n";
+        file << nNodes << "\n";
+        for(int i=0;i<nNodes;++i)
+            file << (i+1) << " " << x[i] << " " << y[i] << " " << z[i] << "\n";
+        file << "$EndNodes\n";
+        file << "$Elements\n";
+        file << nElements << "\n";
+        for(int i=0;i<nElements;++i)
+            file << (i+1) << " 2 2 2 2 " << v1[i] << " " << v2[i] << " " << v3[i] << "\n";
+        file << "$EndElements\n";
+    file.close();
+  }
+  system("rm Ellipsoid.msh");
+  system("mv temp.msh Ellipsoid.msh");
 }
 
 void LDOS_gradient(double lambda_0, double *coeffs, const unsigned int num_coeffs, 
@@ -54,7 +120,7 @@ void LDOS_gradient(double lambda_0, double *coeffs, const unsigned int num_coeff
   //double lambda_0 = 300; //wavelength in unit of nm
   double omega_0 = 2*pi/lambda_0*1e3; //omega in unit of 3e14 rad/sec
   double rho_0 = omega_0*omega_0/2/pi/pi; //free space electric LDOS
-  ofstream omega_file;  //write to file
+  std::ofstream omega_file;  //write to file
   omega_file.open ("OmegaValues.dat");
   omega_file << omega_0;
   omega_file.close();
@@ -62,6 +128,13 @@ void LDOS_gradient(double lambda_0, double *coeffs, const unsigned int num_coeff
   /***************************************************************/
   /* Initialze Spherical Harmonic funciton and Create Mesh *******/
   /***************************************************************/
+  geometry::SphericalHarmonics *sh;  //create spherical harmonic class
+  sh=new geometry::SphericalHarmonics(num_coeffs, coeffs, -1e6, 1e6);
+  double *l=new double[num_coeffs];
+  double *m=new double[num_coeffs];
+  sh->get_lm(l,m);
+
+  if(CompareCoeff(coeffs,num_coeffs)){
   WriteCoeff(coeffs,num_coeffs);
   double el = 0;
   for (int i=0;i<num_coeffs;++i)
@@ -71,12 +144,23 @@ void LDOS_gradient(double lambda_0, double *coeffs, const unsigned int num_coeff
       +std::to_string(num_coeffs)+" "+std::to_string(el)+" Ellipsoid.msh "
       +std::to_string(min_mesh)+" "+std::to_string(max_mesh)+" "+std::to_string(0.01+d_min/1e3);
   system(MeshCommand.c_str()); //create mesh
+  }
+  else{
+    std::vector<double> x,y,z;
+    std::vector<int> v1,v2,v3;
+    ReadMSH(x,y,z,v1,v2,v3);
+    for(int i=0;i<x.size();++i){
+        double theta,phi;
+        sh->theta_phi(x[i],y[i],z[i],theta,phi);
+        double strech = sh->radius(theta,phi)/sqrt(x[i]*x[i]+y[i]*y[i]+z[i]*z[i]);
+        x[i] *= strech;
+        y[i] *= strech;
+        z[i] *= strech;
+    }
+    WriteMSH(x,y,z,v1,v2,v3);
+  }
+
   std::cout << "Mesh complete." << std::endl;
-  geometry::SphericalHarmonics *sh;  //create spherical harmonic class
-  sh=new geometry::SphericalHarmonics(num_coeffs, coeffs, -1e6, 1e6);
-  double *l=new double[num_coeffs];
-  double *m=new double[num_coeffs];
-  sh->get_lm(l,m);
   //std::cout <<"r(0,0): " << sh->radius(0.0,0.0) << std::endl;  
   //std::cout <<"Ylm(0,0,0,0): " << sh->Ylm(0,0,0.0,0.0) << std::endl;  
   /***************************************************************/
@@ -106,6 +190,7 @@ void LDOS_gradient(double lambda_0, double *coeffs, const unsigned int num_coeff
   /***************************************************************/  
   rho_s=0.0;  //electric LDOS at center computed with 3 scatter simulation
   //double *dfdx = new double[num_coeffs]; //gradient initialzed to 0
+  d_min=100;
   for (int i=0;i<num_coeffs-1;++i)
     dfdx[i] = 0;
   /* loop through 3 electric dipole source ***********************/
